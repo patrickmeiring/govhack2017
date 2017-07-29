e795803d08a8c88463288c258518b0f451ba8cf6
Dominic Asley <dominic_asley@hotmail.com>
2017-07-29 13:01:17 +1000

    Initial base API commit

---
+++ EnergyStatisticsAPI/EnergyStatisticsAPI/Controllers/EnergyController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EnergyStatisticsAPI.Controllers
+{
+	[RoutePrefix("energy")]
+	public class EnergyController : ApiController
+	{	
+		[HttpGet]
+		public List<String> Get()
+		{
+			return new List<string>() { "Test1", "Test2" };
+		}
+	}
+}
