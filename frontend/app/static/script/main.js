658597d2c1cbda3872782bc9838fcaacf1b8e8d9
minkwon <s4286260@gmail.com>
2017-07-29 21:54:22 +1000

    add in line chart

--- frontend/app/static/script/main.js
+++ frontend/app/static/script/main.js
@@ -33,71 +33,114 @@ function createChart(title, chartWidth, chartHeight) {
 
 
 $(document).ready(function() {
-	renderLineChart();
 	$.ajax({
 		url: "http://ec2-54-79-91-24.ap-southeast-2.compute.amazonaws.com/api/api/energy",
 		context: document.body
 	}).done(function(data) {
 		renderBarGraph(data);
 	});
+
+	// // this one will fail for now
+	// $.ajax({
+	// 	url: "http://ec2-54-79-91-24.ap-southeast-2.compute.amazonaws.com/api/api/SOMETHING",
+	// 	context: document.body
+	// }).done(function(data) { // change "fail" to "done" once api is ready
+	// 	renderLineChart(data);
+	// });
+	renderLineChart([])
 });
 
 function renderLineChart(data) {
+
+	// MockData overwriting data
+	var data = [
+	{
+		"state" : "QLD",
+		"values" : [
+			{
+				"date" : "2017-01-01",
+				"value" : "10"
+			},
+			{
+				"date" : "2017-01-02",
+				"value" : "20"
+			},
+			{
+				"date" : "2017-01-03",
+				"value" : "20"
+			},
+			{
+				"date" : "2017-01-04",
+				"value" : "40"
+			},
+			{
+				"date" : "2017-01-05",
+				"value" : "25"
+			}
+		]
+	},
+	{
+		"state" : "NSW",
+		"values" : [
+			{
+				"date" : "2017-01-01",
+				"value" : "50"
+			},
+			{
+				"date" : "2017-01-02",
+				"value" : "10"
+			},
+			{
+				"date" : "2017-01-03",
+				"value" : "30"
+			},
+			{
+				"date" : "2017-01-04",
+				"value" : "20"
+			},
+			{
+				"date" : "2017-01-05",
+				"value" : "10"
+			}
+		]
+	}];
+
+	var parseDate = d3.timeParse("%Y-%m-%d");
+
 	var svg = d3.select("#first-chart"),
-    margin = {top: 20, right: 80, bottom: 30, left: 50},
+    margin = {top: 20, right: 80, bottom: 100, left: 50},
     width = svg.attr("width") - margin.left - margin.right,
     height = svg.attr("height") - margin.top - margin.bottom,
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    graph = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 	
 	var xScale = d3.scaleTime().range([0, width]),
 		yScale = d3.scaleLinear().range([height, 0]),
 		z = d3.scaleOrdinal(d3.schemeCategory10);
 
 	var line = d3.line()
-		.curve(d3.curveBasis)
-		.x(function(d) { return xScale(d.date); })
-		.y(function(d) { return yScale(d.power); });
+		.x(function(d) { console.log(d.date); return xScale(parseDate(d.date)); })
+		.y(function(d) { return yScale(+d.value); });
 	
-	var today = new Date(2017, 10, 11);
-	var yesterday = new Date(2017, 10, 10);
-	var cities = [{
-		id: "Queensland",
-		values: [{
-			date: today,
-			power: 10
-		}, {
-			date: yesterday,
-			power: 11
-		}]
-	}];
-
-/*	var cities = data.columns.slice(1).map(function(id) {
-		return {
-		id: id,
-		values: data.map(function(d) {
-			return {date: d.date, temperature: d[id]};
-		})
-		};
-	});*/
-	var dataPoints = [].concat.apply([], cities.map(function (city) {
-		return city.values;//.map(function (point) { point.date });
+	var dataPoints = [].concat.apply([], data.map(function (stateData) {
+		return stateData.values;//.map(function (point) { point.date });
 	}));
 
-	xScale.domain(d3.extent(dataPoints, function(d) { return d.date; }));
+	xScale.domain(d3.extent(dataPoints, function(d) { return parseDate(d.date); }));
 
-	yScale.domain([
-		d3.min(cities, function(c) { return d3.min(c.values, function(d) { return d.power; }); }),
-		d3.max(cities, function(c) { return d3.max(c.values, function(d) { return d.power; }); })
-	]);
+	yScale.domain([10, 50]);
 
-	z.domain(cities.map(function(c) { return c.id; }));
-
-	g.append("g")
+	graph.append("g")
 		.attr("class", "axis axis--x")
 		.attr("transform", "translate(0," + height + ")")
-		.call(d3.axisBottom(xScale));
+		.call(d3.axisBottom(xScale).tickFormat(d3.timeFormat("%Y-%m-%d")))
+		.selectAll("text")
+			.style("text-anchor", "end")
+        	.attr("dx", "-.8em")
+        	.attr("dy", ".15em")
+        	.attr("transform", "rotate(-65)");
 
-	g.append("g")
+
+	graph.append("g")
 		.attr("class", "axis axis--y")
 		.call(d3.axisLeft(yScale))
 		.append("text")
@@ -105,25 +148,24 @@ function renderLineChart(data) {
 		.attr("y", 6)
 		.attr("dy", "0.71em")
 		.attr("fill", "#000")
-		.text("Temperature, ºF");
+		.text("Y AXIS");
 
-	var city = g.selectAll(".city")
-		.data(cities)
-		.enter().append("g")
-		.attr("class", "city");
+	graph.selectAll(".trend-line")
+		.data(data)
+		.enter()
+		.append("path")
+		.attr("stroke", function(d, i) { return z(i); })
+		.attr("fill", "none")
+		.attr("class", "trend-line")
+		.attr("d", function(d) { return line(d.values); });
 
-	city.append("path")
-		.attr("class", "line")
-		.attr("d", function(d) { return line(d.values); })
-		.style("stroke", function(d) { return z(d.id); });
-		
-	city.append("text")
-		.datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
-		.attr("transform", function(d) { return "translate(" + xScale(d.value.date) + "," + yScale(d.value.power) + ")"; })
-		.attr("x", 3)
-		.attr("dy", "0.35em")
-		.style("font", "10px sans-serif")
-		.text(function(d) { return d.id; });
+	// city.append("text")
+	// 	.datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
+	// 	.attr("transform", function(d) { return "translate(" + xScale(d.value.date) + "," + yScale(d.value.power) + ")"; })
+	// 	.attr("x", 3)
+	// 	.attr("dy", "0.35em")
+	// 	.style("font", "10px sans-serif")
+	// 	.text(function(d) { return d.id; });
 }
 
 function renderBarGraph(data) {
