e67c4401983aaaf4b3f6d161cbe715ad85b04232
Dominic Asley <dominic_asley@hotmail.com>
2017-07-29 16:44:19 +1000

    Adds solar hot water installations per postcode

---
+++ EnergyStatisticsAPI/EnergyDataProvider/Models/SolarHotWaterInstallation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnergyDataProvider.Models
+{
+	public class SolarHotWaterInstallation
+	{
+		public string Postcode { get; set; }
+		public string Technology { get; set; }
+		public string TankCapacity { get; set; }
+		public string Booster { get; set; }
+		public string Collector { get; set; }
+		public string CollectorCircuit { get; set; }
+		public string Configuration { get; set; }
+	}
+}
