644f8f4583f353896365b03edcba3ae92a0ba977
patrickmeiring <patrick.meiring@live.com>
2017-07-29 16:38:30 +1000

    Draw a very basic graph

--- frontend/app/static/script/main.js
+++ frontend/app/static/script/main.js
@@ -30,3 +30,97 @@ function createChart(title, chartWidth, chartHeight) {
 
     
 }
+
+
+$(document).ready(function() {
+		renderChart();
+	$.ajax({
+		url: "http://ec2-54-79-91-24.ap-southeast-2.compute.amazonaws.com/api/api/energy",
+		context: document.body
+	}).done(function(data) {
+	});
+});
+
+function renderChart(data) {
+	var svg = d3.select("#first-chart"),
+    margin = {top: 20, right: 80, bottom: 30, left: 50},
+    width = svg.attr("width") - margin.left - margin.right,
+    height = svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	
+	var xScale = d3.scaleTime().range([0, width]),
+		yScale = d3.scaleLinear().range([height, 0]),
+		z = d3.scaleOrdinal(d3.schemeCategory10);
+
+	var line = d3.line()
+		.curve(d3.curveBasis)
+		.x(function(d) { return xScale(d.date); })
+		.y(function(d) { return yScale(d.power); });
+	
+	var today = new Date(2017, 10, 11);
+	var yesterday = new Date(2017, 10, 10);
+	var cities = [{
+		id: "Queensland",
+		values: [{
+			date: today,
+			power: 10
+		}, {
+			date: yesterday,
+			power: 11
+		}]
+	}];
+
+/*	var cities = data.columns.slice(1).map(function(id) {
+		return {
+		id: id,
+		values: data.map(function(d) {
+			return {date: d.date, temperature: d[id]};
+		})
+		};
+	});*/
+	var dataPoints = [].concat.apply([], cities.map(function (city) {
+		return city.values;//.map(function (point) { point.date });
+	}));
+
+	xScale.domain(d3.extent(dataPoints, function(d) { return d.date; }));
+
+	yScale.domain([
+		d3.min(cities, function(c) { return d3.min(c.values, function(d) { return d.power; }); }),
+		d3.max(cities, function(c) { return d3.max(c.values, function(d) { return d.power; }); })
+	]);
+
+	z.domain(cities.map(function(c) { return c.id; }));
+
+	g.append("g")
+		.attr("class", "axis axis--x")
+		.attr("transform", "translate(0," + height + ")")
+		.call(d3.axisBottom(xScale));
+
+	g.append("g")
+		.attr("class", "axis axis--y")
+		.call(d3.axisLeft(yScale))
+		.append("text")
+		.attr("transform", "rotate(-90)")
+		.attr("y", 6)
+		.attr("dy", "0.71em")
+		.attr("fill", "#000")
+		.text("Temperature, ºF");
+
+	var city = g.selectAll(".city")
+		.data(cities)
+		.enter().append("g")
+		.attr("class", "city");
+
+	city.append("path")
+		.attr("class", "line")
+		.attr("d", function(d) { return line(d.values); })
+		.style("stroke", function(d) { return z(d.id); });
+		
+	city.append("text")
+		.datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
+		.attr("transform", function(d) { return "translate(" + xScale(d.value.date) + "," + yScale(d.value.power) + ")"; })
+		.attr("x", 3)
+		.attr("dy", "0.35em")
+		.style("font", "10px sans-serif")
+		.text(function(d) { return d.id; });
+}
